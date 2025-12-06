393e77f21b3aa6424a5d0b13e2f2c3d0011e13a8
Albena09 <albena80.edu@gmail.com>
2025-12-06 12:43:08 +0600

    fix the eyeicon position and source when write something in input box

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 let eyeicon = document.getElementById("eyeicon");
 let password = document.getElementById("password");
 
-//===== function for click feature========
+//===== function for click feature ========
 
 eyeicon.onclick = function () {
-  if (password.type == "password") {
+  if (password.type === "password") {
     password.type = "text";
-    eyeicon.type = "visibility.png";
+    eyeicon.src = "images/visibility.png"; 
   } else {
     password.type = "password";
-    eyeicon.type = "visibility_off.png";
+   eyeicon.src = "images/visibility_off.png";
   }
 };
